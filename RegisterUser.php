5c684bccec8cf558b49c32e95a6684f2f2701f24
Ingo Sigmund <ingo.sigmund@web.de>
2015-04-01 22:04:03 +0200

    validate name email and show error accordingly

--- RegisterUser.php
+++ RegisterUser.php
@@ -10,6 +10,32 @@ if(!empty($_POST['submitted']))
 		$name_error="no name given";
 	}
 
+	$validation_error = false;
+
+	require "functions.php";
+	//Set user's data while escaping to avoid SQL Injection
+	$user_id = mysql_escape_mimic($_POST['u']);
+	$user_email = mysql_escape_mimic($_POST['e']);
+	$user_real = mysql_escape_mimic($_POST['r']);
+	$user_pass = mysql_escape_mimic($_POST['p']);
+
+	// check validity of real name
+	if (!preg("/^[a-zA-Z ]*$/", $user_real)) {
+		$validation_error = true;
+		$validation_error_text = "Kein gültiger Vor- und Nachname eingegeben !";
+	}
+
+	// check validity of email
+	if(!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
+		$validation_error = true;
+		$validation_error_text = "Keine gültige eMail-Adresse eingegeben !";		
+	}
+
+
+	// check valid reCAPTCHA response
+
+	//
+
 }
 ?>
 
@@ -19,8 +45,8 @@ if(!empty($_POST['submitted']))
 <!--[if IE 8]><html class="ng-csp ie ie8 lte9 lte8" data-placeholder-focus="false" lang="de" ><![endif]-->
 <!--[if IE 9]><html class="ng-csp ie ie9 lte9" data-placeholder-focus="false" lang="de" ><![endif]-->
 <!--[if gt IE 9]><html class="ng-csp ie" data-placeholder-focus="false" lang="de" ><![endif]-->
-<!--[if !IE]><!--><html class="ng-csp" data-placeholder-focus="false" lang="de" ><!--<![endif]-->
-
+<!--[if !IE]><!-->
+<html class="ng-csp" data-placeholder-focus="false" lang="de" ><!--<![endif]-->
 	<head>
 		<title>
 		OwnCloud Waldorfkindergarten Deggenhausertal</title>
@@ -67,100 +93,113 @@ if(!empty($_POST['submitted']))
 		<noscript><div id="nojavascript"><div>Diese Anwendung benötigt ein aktiviertes JavaScript zum korrekten Betrieb.  Bitte <a href="http://enable-javascript.com/" target="_blank">aktiviere JavaScript</a> und lade diese Seite neu.</div></div></noscript>
 		<div class="wrapper"><!-- for sticky footer -->
 			<div class="v-align"><!-- vertically centred box -->
-									<header>
-						<div id="header">
-							<div class="logo svg">
-								<h1 class="hidden-visually">
-									OwndCloud								</h1>
-							</div>
-							<div id="logo-claim" style="display:none;"></div>
+				<header>
+					<div id="header">
+						<div class="logo svg">
+							<h1 class="hidden-visually">OwndCloud</h1>
 						</div>
-					</header>
+						<div id="logo-claim" style="display:none;"></div>
+					</div>
+				</header>
 								
-<!--[if IE 8]><style>input[type="checkbox"]{padding:0;}</style><![endif]-->
-<!-- registration.php -->
-<form method="post" action="?" name="login">
-	<fieldset>
-		<p id="message" class="hidden">
-			<img class="float-spinner" alt=""
-				src="/core/img/loading-dark.gif" />
-			<span id="messageText"></span>
-			<!-- the following div ensures that the spinner is always inside the #message div -->
-			<div style="clear: both;"></div>
-		</p>
-		<p class="grouptop">
-			<input type="text" name="r" id="newuserfullname"
-			    style="width: 255px; padding-left: 36px"
-				placeholder="Vor- und Nachname"
-				value=""
-				autofocus
-				autocomplete="on" autocapitalize="off" autocorrect="off" required />
-			<label for="newuserfullname" class="infield">Vor- und Nachname</label>
-			<!-- <img class="svg" src="/core/img/actions/user.svg" alt=""/> -->
-		</p>
-	
-		<p class="groupbottom">
-			<input type="text" name="e" id="newuseremail" value=""
-					    style="width: 255px; padding-left: 36px"
-				placeholder="eMail"
-		        autocomplete="on" autocapitalize="off" autocorrect="off" required />
-			<label for="newuseremail" class="infield">eMail</label>
-			<!-- <img class="svg" src="/core/img/actions/user.svg" alt=""/>  -->
-		</p>
-		
-		
-		<p class="grouptop">
-			<input type="text" name="u" id="newusername" value=""
-					    style="width: 255px; padding-left: 36px"
-				placeholder="Benutzername"
-		        autocomplete="on" autocapitalize="off" autocorrect="off" required />
-			<label for="newusername" class="infield">Benutzername</label>
-			<img class="svg" style="top: 22px;" id="password-icon" src="/core/img/actions/user.svg" alt=""/> 
-		</p>
-		
-		<p class="groupbottom">
-			<input type="password" name="p" id="newuserpassword" value=""
-					    style="width: 255px; padding-left: 36px"
-				placeholder="Passwort"
-		        autocomplete="on" autocapitalize="off" autocorrect="off" required />
-			<label for="newuserpassword" class="infield">Passwort</label>
-			<img class="svg" id="password-icon" src="/core/img/actions/password.svg" alt=""/>
-		</p>
-		
-		<div class="g-recaptcha" data-sitekey="6Ld6ogQTAAAAADzkW1e_w-ymWcoPnn_PF1mzYGSi"></div>
-		
-		<p>
-		  <input style="width: 304px; margin-top: 20px" type="submit" value="Benutzer registrieren">
-        </p>
+				<!--[if IE 8]><style>input[type="checkbox"]{padding:0;}</style><![endif]-->
+				<form method="post" action="?" name="login">
+					<fieldset>
+						<p id="message" class="hidden">
+							<img class="float-spinner" alt="" src="/core/img/loading-dark.gif" />
+							<span id="messageText"></span>
+							<!-- the following div ensures that the spinner is always inside the #message div -->
+							<div style="clear: both;"></div>
+						</p>
+						<p class="grouptop">
+							<input
+								type="text"
+								name="r" 
+								id="newuserfullname"
+							    style="width: 255px; padding-left: 36px"
+								placeholder="Vor- und Nachname"
+								value="<?php echo $user_real; ?>"
+								autofocus
+								autocomplete="on" 
+								autocapitalize="off" 
+								autocorrect="off" 
+								required 
+							/>
+							<label for="newuserfullname" class="infield">Vor- und Nachname</label>
+						</p>
+					
+						<p class="groupbottom">
+							<input 
+								type="text" 
+								name="e" 
+								id="newuseremail" 
+								value="<?php echo $user_email; ?>"
+								style="width: 255px; padding-left: 36px"
+								placeholder="eMail"
+						        autocomplete="on" 
+						        autocapitalize="off" 
+						        autocorrect="off" 
+						        required 
+						     />
+							<label for="newuseremail" class="infield">eMail</label>
+						</p>
+						
+						
+						<p class="grouptop">
+							<input 
+								type="text" 
+								name="u" 
+								id="newusername" 
+								value="<?php echo $user_id; ?>"
+								style="width: 255px; padding-left: 36px"
+								placeholder="Benutzername"
+						        autocomplete="on" 
+						        autocapitalize="off" 
+						        autocorrect="off" 
+						        required />
+							<label for="newusername" class="infield">Benutzername</label>
+							<img class="svg" style="top: 22px;" id="password-icon" src="/core/img/actions/user.svg" alt=""/> 
+						</p>
+						
+						<p class="groupbottom">
+							<input 
+								type="password" 
+								name="p" 
+								id="newuserpassword" 
+								value=""
+								style="width: 255px; padding-left: 36px"
+								placeholder="Passwort"
+						        autocomplete="on" 
+						        autocapitalize="off" 
+						        autocorrect="off" 
+						        required />
+							<label for="newuserpassword" class="infield">Passwort</label>
+							<img class="svg" id="password-icon" src="/core/img/actions/password.svg" alt=""/>
+						</p>
+						
+						<!-- reCAPTCHA widget -->
+						<div class="g-recaptcha" data-sitekey="6Ld6ogQTAAAAADzkW1e_w-ymWcoPnn_PF1mzYGSi"></div>
+						
+						<!-- Submit button -->
+						<p><input style="width: 304px; margin-top: 20px" type="submit" value="Benutzer registrieren"></p>
 
-<?php
-echo 'PHP';
-if (isset($_POST['err'])){
-  $errorcode = $_POST['err'];
-  echo $errorcode;
-}	
-?>
-	
-<ul>
-    <li class="error"><?php echo($name_error); ?>
-        <br></br>
-        <p class="hint">Das ausgewählte Dokument wurde auf dem Server nich…
-        </p>
-        <p class="hint">
-            <a href="/index.php">Du kannst zur Rückkehr zu OwndCloud hier klicken.
-            </a>
-        </p>
-    </li>
-</ul>
-			
-	</fieldset>
-</form>
+						<!-- error handling -->
+						<?php
+							if ($validation_error) {
+								echo "<ul><li class='error'>";
+								echo $validation_error_text; 
+							
 
+						        //<br></br>
+						        //<p class="hint">Das ausgewählte Dokument wurde auf dem Server nich…</p>
+						        //<p class="hint"><a href="/index.php">Du kannst zur Rückkehr zu OwndCloud hier klicken.</a></p>
 
-
-
-
-
+						        echo "</li></ul>"
+						    }    
+						?>
+							
+					</fieldset>
+				</form>
 				<div class="push"></div><!-- for sticky footer -->
 			</div>
 		</div>
